c72fbf95ba0e1f968bbe550078b46bd362c8d0fe
wn1980 <waipotn@hotmail.com>
2021-08-30 11:27:52 +0700

    first commit

---
+++ udev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo  'KERNEL=="ttyACM*", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="5740", MODE:="0777", GROUP:="dialout",  SYMLINK+="sensor_imu"' > /etc/udev/rules.d/88-sensor_imu_module.rules
+
+service udev reload
+sleep 1
+service udev restart
