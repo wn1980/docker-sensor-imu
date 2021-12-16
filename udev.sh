5196f15924346f70019400a8fbca3a783545e845
wn1980 <waipotn@hotmail.com>
2021-12-17 06:22:35 +0700

    build on arm64

--- udev.sh
+++ udev.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -e
 
 echo  'KERNEL=="ttyACM*", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="5740", MODE:="0777", GROUP:="dialout",  SYMLINK+="sensor_imu"' > /etc/udev/rules.d/88-sensor_imu_module.rules
 
